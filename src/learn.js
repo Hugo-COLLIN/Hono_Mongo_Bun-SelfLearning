26649e26cbc01aac490d005e2feb1fe2799f7f49
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-08 01:07:56 +0200

    Update and Delete video by ID - https://youtu.be/AcQm4x5dxeU?t=2602

--- src/learn.js
+++ src/learn.js
@@ -35,4 +35,38 @@ app.get('/videos', (c) => {
   })
 })
 
+//Read by ID
+app.get('/video/:id', (c) => {
+  const {id} = c.req.param()
+  const video = videos.find(video => video.id === id)
+  if (!video)
+    return c.json({message: 'Video not found'}, 404)
+  return c.json(video)
+});
+
+// Update by ID
+app.put('/video/:id', async (c) => {
+  const {id} = c.req.param()
+  const index = videos.findIndex((video) => video.id === id)
+  if (index === -1)
+    return c.json({message: 'Video not found'}, 404)
+  const {videoName, channelName, duration} = await c.req.json()
+
+  videos[index] = {
+    ...videos[index],
+    videoName,
+    channelName,
+    duration
+  }
+
+  return c.json(videos[index])
+})
+
+// Delete by ID
+app.delete('/video/:id', (c) => {
+  const {id} = c.req.param()
+  videos = videos.filter(video => video.id !== id)
+  return c.json({message: 'Video deleted'})
+})
+
 export default app;

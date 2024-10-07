12ae17ccf2ac8c228d006344653c81e858d0dd5c
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-08 00:42:28 +0200

    POST video - https://youtu.be/AcQm4x5dxeU?t=1730

---
+++ src/learn.js
@@ -0,0 +1,27 @@
+import { Hono } from 'hono';
+import { v4 as uuidv4 } from 'uuid';
+
+let videos = []
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.html('<h1>Welcome to Hono!</h1>');
+})
+
+app.post('/video', async (c) => {
+  const {videoName, channelName, duration} = await c.req.json();
+
+  const newVideo = {
+    id: uuidv4(),
+    videoName,
+    channelName,
+    duration
+  }
+
+  videos.push(newVideo);
+
+  return c.json(newVideo);
+})
+
+export default app;
